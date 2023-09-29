1fd5e9cbd3d9672735259a65685c802f8225dd97
Noah <noahhughesph@gmail.com>
2023-09-29 01:34:06 -0400

    all tests passing

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,7 @@
 def oxford_comma(items):
-    return None
+    if (len(items) < 2):
+        return "".join(items)
+    if (len(items) == 2):
+        return " and ".join(items)
+    last_word = items.pop(-1)
+    return (f'{", ".join(items)}, and {last_word}')
